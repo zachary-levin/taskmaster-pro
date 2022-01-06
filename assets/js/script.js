f98362b53dfdb38d9721cf65dfff1af84e69acc0
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-01-06 17:46:28 -0600

    optimized code and added more  comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,45 +82,45 @@ var taskP = $("<p>")
 $(this).replaceWith(taskP);
 });
 
+// enable draggable/sortable feature on list-group elements
 $(".card .list-group").sortable({
+  // enable dragging across lists
   connectWith: $(".card .list-group"),
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function (event) {
-    console.log("activate", this);
+  activate: function(event, ui) {
+    console.log(ui);
   },
-  deactivate: function(event) {
-    console.log("deactivate", this);
+  deactivate: function(event, ui) {
+    console.log(ui);
   },
   over: function(event) {
-    console.log("over", event.target);
+    console.log(event);
   },
   out: function(event) {
-    console.log("out", event.target);
+    console.log(event);
   },
-  update: function(event) {
-    // array to store the task data in
+  update: function() {
     var tempArr = [];
 
     // loop over current set of children in sortable list
-    $(this).children().each(function() {
-      var text = $(this)
-        .find("p")
-        .text()
-        .trim();
-
-      var date = $(this)
-        .find("span")
-        .text()
-        .trim();
-
-        // add task data to the temp array as an object
+    $(this)
+      .children()
+      .each(function() {
+        // save values in temp array
         tempArr.push({
-          text: text,
-          date: date
+          text: $(this)
+            .find("p")
+            .text()
+            .trim(),
+          date: $(this)
+            .find("span")
+            .text()
+            .trim()
         });
-    });
+      });
+
     // trim down list's ID to match object property
     var arrName = $(this)
       .attr("id")
@@ -129,22 +129,26 @@ $(".card .list-group").sortable({
     // update array on tasks object and save
     tasks[arrName] = tempArr;
     saveTasks();
-    
+  },
+  stop: function(event) {
+    $(this).removeClass("dropover");
   }
 });
 
+// trash icon can be dropped onto
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui) {
+    // remove dragged event from the DOM
     ui.draggable.remove();
-    console.log("drop");
+
   },
   over: function(event, ui) {
-    console.log("over");
+    console.log(ui);
   },
   out: function(event, ui) {
-    console.log("out");
+    console.log(ui);
   }
 });
 
